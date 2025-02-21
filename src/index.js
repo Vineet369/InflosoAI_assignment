0bc0bd1b0958cc9a7e4432c1020b1912ce1e7a6a
Vineet Maurya <101665909+Vineet369@users.noreply.github.com>
2025-02-21 17:58:27 +0530

    feat: connect to mongodb database and add utilities

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'; 
+import connectDB from './ db/index.db.js';
+import { app } from './app.js';
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("error in app ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8001, () => {
+        console.log(`app running on port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection error", err);
+})

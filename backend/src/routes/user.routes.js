da5641c16e5858230cc89ce564b425f2eff7ecca
Vineet Maurya <101665909+Vineet369@users.noreply.github.com>
2025-02-22 10:19:42 +0530

    feat add routes for email verificarion and restructure folders

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken, changePassword, getCurrentUser,   
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changePassword, getCurrentUser, sendVerifyOtp, verifyEmail, sendResetOtp,   
     } from "../controllers/user.controllers.js"; 
 import { verifyToken } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -20,6 +20,10 @@ router.route("/logout").post(verifyToken,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyToken,changePassword)
 router.route("/current-user").get(verifyToken,getCurrentUser)
+router.route("/send-verify-otp").get(verifyToken,sendVerifyOtp)
+router.route("/verify-account").get(verifyToken,verifyEmail)
+router.route("/send-reset-otp").get(verifyToken,sendResetOtp)
+
 
 
 export default router;
